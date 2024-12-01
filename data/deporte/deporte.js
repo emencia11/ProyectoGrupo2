488c6a515d56a11dffee93754c4b25944562f895
emencia11 <err_menciaf@unicah.edu>
2024-12-01 10:37:34 -0600

    EM: Se agrega page de tienda virtula.

--- data/deporte/deporte.js
+++ data/deporte/deporte.js
@@ -1,9 +1,125 @@
 export default {
     deporte: [
-        {Nombre: 'Soccer', Descripcion: 'Juego con Pelota', Condiciones:'Amor', Ranking:'Excelente'},
-        {Nombre: 'Baloncesto', Descripcion: 'Juego con Pelota', Condiciones:'Balón, uniforme, zapatillas', Ranking:'Excelente'},
-        {Nombre: 'Tenis', Descripcion: 'Deporte de raqueta qué se juega en dos jugadores', Condiciones:'Raqueta, pekota, zapatillas', Ranking:'Excelente'},
-        {Nombre: 'Tenis', Descripcion: 'Deporte de raqueta qué se juega en dos jugadores', Condiciones:'Raqueta, pekota, zapatillas', Ranking:'Excelente'}
-
+      {
+        Nombre: 'Pesas',
+        Descripcion: 'Pesas de 10kg.',
+        Ranking: 'Excelente',
+        Imagen: 'https://img.freepik.com/psd-gratis/vista-arriba-sobre-manija-aislada_23-2151849386.jpg?t=st=1732944458~exp=1732948058~hmac=edc93b92e5af8ca3be90e4ba58f14f6b072a57295f613615d18e9c2cd6cde44e&w=996'
+      },
+      {
+        Nombre: 'Banco de Pesas',
+        Descripcion: 'Banco de pesas ajustable para ejercicios de fuerza.',
+        Ranking: 'Excelente',
+        Imagen: 'https://img.freepik.com/foto-gratis/equipo-gimnasio-3d_23-2151114151.jpg?t=st=1733019602~exp=1733023202~hmac=64b4dd92411a97808864b5a8a8acd285834080c64fdcfd789e042ba2c41fcbc6&w=996'
+      },
+      {
+        Nombre: 'Bicicleta Estática',
+        Descripcion: 'Bicicleta estática con pantalla LCD.',
+        Ranking: 'Muy Bueno',
+        Imagen: 'https://img.freepik.com/fotos-premium/bicicleta-estatica-aislada_90839-443.jpg?w=740 '
+      },
+      {
+        Nombre: 'Proteína Whey',
+        Descripcion: 'Proteína Whey de 2kg, sabor chocolate.',
+        Ranking: 'Excelente',
+        Imagen: 'https://img.freepik.com/vector-gratis/envases-nutricion-deportiva-sumbbells_1284-6583.jpg?t=st=1733020144~exp=1733023744~hmac=8d98aa8ace43a22e3b558499f4e96eae2d08e5df25f1722b624e7e0f25e1c343&w=740'
+      },
+      {
+        Nombre: 'Soporte para Pesas',
+        Descripcion: 'Soporte para almacenamiento de pesas.',
+        Ranking: 'Excelente',
+        Imagen: 'https://img.freepik.com/vector-gratis/barra-deportiva-metal-rejilla_1284-58534.jpg?t=st=1733021021~exp=1733024621~hmac=16812c5d258d3190d5b3b7dd256c28e7c04d0284f79dfeeecf17361060b4ae07&w=740'
+      },
+      {
+        Nombre: 'Mat de Yoga',
+        Descripcion: 'Mat de yoga antideslizante de 5mm.',
+        Ranking: 'Excelente',
+        Imagen: 'https://img.freepik.com/vector-gratis/iconos-estera-alfombra-yoga-naranja-realista-establecen-ilustracion-vectorial-aislada_1284-79042.jpg?t=st=1733021182~exp=1733024782~hmac=a63a2548b4c896d58be342fd313e7783fc81231f965cf8de9752a2aaa2806921&w=900'
+      },
+      {
+        Nombre: 'Kettlebell',
+        Descripcion: 'Kettlebell de 15kg.',
+        Ranking: 'Excelente',
+        Imagen: 'https://img.freepik.com/psd-gratis/primer-plano-gray-kettlebell_23-2151867238.jpg?t=st=1733021268~exp=1733024868~hmac=76643c57b357b86874ae95bec09ed59f490b697e5925110aa93a5011b927f9b5&w=740'
+      },
+      {
+        Nombre: 'Barras para dominadas',
+        Descripcion: 'Barras para hacer dominadas en casa.',
+        Ranking: 'Excelente',
+        Imagen: 'https://img.freepik.com/fotos-premium/barra-traccion-mangos-espuma-aislados-sobre-fondo-blanco_868783-67905.jpg?w=740'
+      },
+      {
+        Nombre: 'Caminadora',
+        Descripcion: 'Caminadora de correr para entrenamiento cardiovascular.',
+        Ranking: 'Excelente',
+        Imagen: 'https://img.freepik.com/fotos-premium/maquina-cinta-correr-equipo-gimnasia-acondicionamiento-fisico_274626-11330.jpg?w=740'
+      },
+      {
+        Nombre: 'Shaker para batidos',
+        Descripcion: 'Shaker con compartimento para proteínas y mezclador.',
+        Ranking: 'Excelente',
+        Imagen: 'https://img.freepik.com/vector-gratis/botella-plata-vacia-realista-proteina-mezclador-o-agitador_88138-261.jpg?t=st=1733025260~exp=1733028860~hmac=77173104fe7bf45bafd90f7ee35596dba7ddf47508cf410015df1a35b4870208&w=740'
+      },
+      {
+        Nombre: 'Guantes de entrenamiento',
+        Descripcion: 'Guantes con soporte para muñeca y agarre antideslizante.',
+        Ranking: 'Muy Bueno',
+        Imagen: 'https://img.freepik.com/fotos-premium/ponga-guantes-sobre-fondo-blanco_268174-335.jpg?w=996'
+      },
+      {
+        Nombre: 'Rodillera deportiva',
+        Descripcion: 'Rodillera con soporte para proteger las articulaciones durante el ejercicio.',
+        Ranking: 'Muy Bueno',
+        Imagen: 'https://img.freepik.com/fotos-premium/protectores-patinaje-rodillas-aisladas-sobre-fondo-blanco_1014317-2533.jpg?w=826'
+      },
+      {
+        Nombre: 'Rueda para abdominales',
+        Descripcion: 'Rueda con doble agarre para ejercicios abdominales.',
+        Ranking: 'Muy Bueno',
+        Imagen: 'https://img.freepik.com/fotos-premium/rueda-ejercicio-azul-sobre-fondo-blanco_61975-18.jpg?w=996'
+      },
+      {
+        Nombre: 'Mancuernas ajustables',
+        Descripcion: 'Mancuernas con peso ajustable de 2 a 10kg.',
+        Ranking: 'Excelente',
+        Imagen: 'https://img.freepik.com/foto-gratis/grandes-pesas-sobre-blanco_144627-24208.jpg?t=st=1733025720~exp=1733029320~hmac=77bdf96b46e0f36422e648045b990503dc229514f10d133ca6e79b75cca358a5&w=996'
+      },
+      {
+        Nombre: 'Suplemento de BCAA',
+        Descripcion: 'Aminoácidos de cadena ramificada para recuperación muscular.',
+        Ranking: 'Bueno',
+        Imagen: 'https://img.freepik.com/vector-gratis/kettlebell-recipiente-batido-proteinas_1284-6584.jpg?t=st=1733025813~exp=1733029413~hmac=a56c0de6dac921dcbc5080d9b6c7c91bfd392e5da71279392bdaf80b159c90f1&w=740'
+      },
+      {
+        Nombre: 'Suplemento de omega-3',
+        Descripcion: 'Suplemento de ácidos grasos omega-3 para la salud cardiovascular.',
+        Ranking: 'Muy Bueno',
+        Imagen: 'https://img.freepik.com/vector-gratis/frascos-medicina-capsulas-color-amarillo-claro-aislado-blanco_107791-5015.jpg?t=st=1733026670~exp=1733030270~hmac=6a555cc6ee589843ec03f362d6bb7ed25a297429b423d83812518d02448c9899&w=996'
+      },
+      {
+        Nombre: 'Bolsa de boxeo',
+        Descripcion: 'Bolsa de boxeo para entrenamiento de fuerza y resistencia.',
+        Ranking: 'Muy Bueno',
+        Imagen: 'https://img.freepik.com/fotos-premium/saco-boxeo-modelado-3d_274824-912.jpg?w=740'
+      },
+      {
+        Nombre: 'Cinturón de levantamiento de pesas',
+        Descripcion: 'Cinturón para apoyo lumbar durante levantamientos pesados.',
+        Ranking: 'Muy Bueno',
+        Imagen: 'https://img.freepik.com/fotos-premium/gimnasio-cinturon_1056055-657.jpg?w=740'
+      },
+      {
+        Nombre: 'Máquina elíptica',
+        Descripcion: 'Máquina elíptica para entrenamiento cardiovascular de bajo impacto.',
+        Ranking: 'Muy Bueno',
+        Imagen: 'https://img.freepik.com/fotos-premium/equipo-ejercicio-eliptico_719385-555.jpg?w=740'
+      },
+      {
+        Nombre: 'Suplemento de creatina',
+        Descripcion: 'Suplemento para mejorar el rendimiento y la recuperación muscular.',
+        Ranking: 'Excelente',
+        Imagen: 'https://img.freepik.com/vector-gratis/conjunto-realista-dos-pesas-deporte-complementan-superficie-vidriosa-fondo-blanco-ilustracion-vectorial_1284-19886.jpg?t=st=1733027514~exp=1733031114~hmac=081c742deb5f73ff333b315c345f08ddd2d1c9138ae153d0a2862c5f46e29371&w=740'
+      },
     ]
-}
+  }
+  
